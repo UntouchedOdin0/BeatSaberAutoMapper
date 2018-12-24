397aeba7196a7aa0617bf6349a32fea90dee2fd4
UntouchedOdin0 <UntouchedOdin0@users.noreply.github.com>
2018-12-24 12:21:01 +0000

    Just pushing the files
    here are the initial files for the software, it may be rubbish coding but hey i started 3 days ago ;)

---
+++ BeatSaberAutoMapper/BeatSaberAutoMapper.Designer.cs
@@ -0,0 +1,124 @@
+namespace BeatSaberAutoMapper
+{
+    partial class BeatSaberAutoMapper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.debugInstructionsLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.helloWorldLabel = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(127, 228);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(129, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Click here for github Page";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // debugInstructionsLabel
+            // 
+            this.debugInstructionsLabel.AutoSize = true;
+            this.debugInstructionsLabel.Location = new System.Drawing.Point(214, 75);
+            this.debugInstructionsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.debugInstructionsLabel.Name = "debugInstructionsLabel";
+            this.debugInstructionsLabel.Size = new System.Drawing.Size(122, 13);
+            this.debugInstructionsLabel.TabIndex = 1;
+            this.debugInstructionsLabel.Text = "Load the song file below";
+            this.debugInstructionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.debugInstructionsLabel.Click += new System.EventHandler(this.debugInstructionsLabel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 156);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Click to load song";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // helloWorldLabel
+            // 
+            this.helloWorldLabel.AutoSize = true;
+            this.helloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloWorldLabel.Location = new System.Drawing.Point(111, 25);
+            this.helloWorldLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.helloWorldLabel.Name = "helloWorldLabel";
+            this.helloWorldLabel.Size = new System.Drawing.Size(373, 26);
+            this.helloWorldLabel.TabIndex = 3;
+            this.helloWorldLabel.Text = "Welcome to Beat Saber Auto Mapper";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(127, 252);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(170, 13);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Click here for help with Beat Saber";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // BeatSaberAutoMapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.helloWorldLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.debugInstructionsLabel);
+            this.Controls.Add(this.linkLabel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "BeatSaberAutoMapper";
+            this.Text = "Beat Saber Auto Mapper";
+            this.Load += new System.EventHandler(this.BeatSaberAutoMapper_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label debugInstructionsLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label helloWorldLabel;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ BeatSaberAutoMapper/BeatSaberAutoMapper.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+namespace BeatSaberAutoMapper
+{
+    public partial class BeatSaberAutoMapper : Form
+    {
+        public BeatSaberAutoMapper()
+        {
+            InitializeComponent();
+
+            this.MinimumSize = new Size(600, 480);
+            this.MaximumSize = new Size(600, 480);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Click on the link below to continue learning how to build a desktop app using WinForms!
+            System.Diagnostics.Process.Start("https://github.com/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog songFile = new OpenFileDialog();
+
+            songFile.Filter = "Beat Saber Audio File|*.ogg";
+            songFile.Title = "Select the audio file";
+            songFile.ShowDialog();
+
+            //OpenFileDialog fdlg = new OpenFileDialog();
+            //fdlg.Title = "Find your .ogg Song Path!";
+            //fdlg.InitialDirectory = @"c:\";
+            //fdlg.Filter = "All files (*.*) |*.*|All Files (*.*)|*.*)";
+            //fdlg.FilterIndex = 2;
+            //fdlg.RestoreDirectory = true;
+            //if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void BeatSaberAutoMapper_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void debugInstructionsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discordapp.com/invite/beatsabermods");
+        }
+    }
+}
